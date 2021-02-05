1cddcf23d2cca4516dd0fa0b1e7400863435b1f4
Daniel Reiling <daniel.reiling@gmail.com>
2021-02-05 09:09:14 -0600

    initial commit

---
+++ IngrediantChecker/IngrediantCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IngrediantCheckerApp.swift
+//  IngrediantChecker
+//
+//  Created by Daniel Reiling on 2/5/21.
+//
+
+import SwiftUI
+
+@main
+struct IngrediantCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
